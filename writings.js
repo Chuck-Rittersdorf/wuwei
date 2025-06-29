b379d89f1bb458dcc4ca01e7b3decbda9aae7c54
Chuck-Rittersdorf <Chuck-Rittersdorf@users.noreply.github.com>
2025-06-29 13:04:02 +0200

    Add new poetry piece "Not Suitable for Publication" in writings.js
    Introduced a new entry in the writingsData array,

--- writings.js
+++ writings.js
@@ -33,12 +33,57 @@ const writingsData = [
     },
     {
         id: 2,
+        title: "Not Suitable for Publication",
+        date: "2025-01-28",
+        type: "poetry",
+        content: `I'm no good at protesting
+and horrible at changing bad habits,
+so what am I to do to alleviate the mess?
+
+I sit on the wobbly chair on the grass.
+I drink coffee; at minimum,
+I do not want to be a nuisance.
+
+A blackbird makes me laugh
+by singing a tune from a discontinued phone.
+
+Is waiting harmful?
+The brain uses about 20 watts,
+whether in thought or idle.
+Most of its work is not
+suitable for publication.
+
+I come up with a book title:
+North of the North Pole,
+subtitled:
+How a Pre-Geometric Phase
+Ends in a Big Bang.
+
+I write papers on the porous in-group
+out-group boundaries
+as an argument
+for early human migration success.
+
+I feel my finger bones tingle,
+as if a large cat is about to jump on me.
+I see a snail on the garden path.
+Even if it makes it to the other side,
+my neighbour smashes them with stones
+to keep her sweet peas safe.
+
+The blackbird flies into the hedge,
+where it stays silent.
+My mother calls me —
+the newspaper has arrived.`
+    },
+    {
+        id: 3,
         title: "Placeholder",
         date: "2025-01-15",
         type: "essay",
         content: 'placeholder'},
     {
-        id: 3,
+        id: 4,
         title: "Placeholder",
         date: "2025-01-05",
         type: "prose",
